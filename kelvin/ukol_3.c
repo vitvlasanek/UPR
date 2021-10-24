5f9894642a3780a85e01accaf42bff91555bbb71
Vít Vlašánek <73115311+vitvlasanek@users.noreply.github.com>
2021-10-24 14:11:55 +0200

    funkční verze 3. úkolu v kelvinu

---
+++ kelvin/ukol_3.c
@@ -0,0 +1,80 @@
+/* 
+Vít Vlašánek
+VŠB - FEI
+ID: VLA0095
+úkol 3 - softmax 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//funkce na sečtení prvku v poli
+float suma_pole(const float * vstupni_vektor, int len){
+	float suma = 0;
+
+	for (int i = 0; i < len; i++){
+		suma = suma + (exp(vstupni_vektor[i]));
+	}	
+	return suma;
+
+}
+
+//funkce na zjištění největšího prvku pole
+int argmax( const float * nove_pole, const int len ){
+    int maximum_pozice = 0;
+    float maximum_hodnota = 0;
+    for (int i = 0; i < len; i++){
+        if (nove_pole[i] > maximum_hodnota){
+            maximum_hodnota = nove_pole [i];
+            maximum_pozice = i;
+        }
+    }
+    return maximum_pozice;    
+}
+
+//funkce softmax
+float * softmax( const float * vstupni_vektor, const int len ){
+    float * nove_pole = (float*)malloc(sizeof(float)*len);
+
+    if (nove_pole == NULL){
+        fprintf(stderr, "Pamet nebyla alokovana\n");
+        exit(-1);
+    }
+    
+    for (int i = 0; i < len; i++){
+        nove_pole[i] = exp(vstupni_vektor[i]) / suma_pole(vstupni_vektor, len);
+    }
+
+    printf("\n%d", argmax(nove_pole, len));
+    for (int i = 0; i < len; i++){
+        printf(" %.4f", nove_pole[i]);
+    }
+    free(nove_pole);
+    return 0;
+}
+
+int main() {
+    int pocet_radku = 0;
+    scanf("%d",&pocet_radku);
+    int len = 10;
+    float * vstupni_vektor = (float*)malloc(sizeof(float)*len);
+    //chyba
+    if (vstupni_vektor == NULL) {
+        fprintf(stderr, "Pamet nebyla alokovana\n");
+    return -1;
+    }
+
+    for (int i = 0; i < pocet_radku; i++)
+    {
+        for (int i = 0; i < len; i++)
+        {
+            scanf("%f", &vstupni_vektor[i]);
+        }
+        
+        softmax(vstupni_vektor,len);
+    }
+    printf("\n");
+    free(vstupni_vektor);
+    return 0;
+}
