288f21dafde57a6d94fc9ccbe42944fa02a63ded
Vít Vlašánek <73115311+vitvlasanek@users.noreply.github.com>
2022-01-12 01:12:01 +0100

    Update main.c

--- Tetris/main.c
+++ Tetris/main.c
@@ -1,6 +1,6 @@
-#include <SDL2/SDL.h>
-#include <stdbool.h>
 #include "mydefs.h"
+#include <stdbool.h>
+
 
 int main()
 {
@@ -8,17 +8,7 @@ int main()
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
         return 1;
     }
- 
-    // Vytvoření okna
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL experiments",  // Název
-        100,                // Souřadnice x
-        100,                // Souřadnice y
-        GAME_WIDTH,                // Šířka
-        GAME_HEIGHT,                // Výška
-        SDL_WINDOW_SHOWN    // Okno se má po vytvoření rovnou zobrazit
-    );
-
+    SDL_Window* window = SDL_CreateWindow("SDL experiments", 100, 100, GAME_WIDTH, GAME_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
         fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
         SDL_Quit();
@@ -33,29 +23,35 @@ int main()
         return 1;
     }
 
-    SDL_Event e;
-    bool quit = false;
-    int pos = 100;
+    // SDL_Event e;
+    // bool quit = false;
+    // int pos = 100;
 
-    while (!quit)
-    {
-        while (SDL_PollEvent(&e))
-        {
-            if (e.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-        }
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Nastavení barvy na černou
-        SDL_RenderClear(renderer);                      // Vykreslení pozadí
+    // while (!quit)
+    // {
+    //     while (SDL_PollEvent(&e))
+    //     {
+    //         if (e.type == SDL_QUIT)
+    //         {
+    //             quit = true;
+    //         }
+    //     }
+    //     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Nastavení barvy na černou
+    //     SDL_RenderClear(renderer);                      // Vykreslení pozadí
 
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Nastavení barvy na červenou
-        SDL_RenderDrawLine(renderer, pos, pos, pos + 100, pos + 100); // Vykreslení čáry
+    //     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Nastavení barvy na červenou
+    //     SDL_RenderDrawLine(renderer, pos, pos, pos + 100, pos + 100); // Vykreslení čáry
 
-        pos++;
+    //     pos++;
 
-        SDL_RenderPresent(renderer);  // Prezentace kreslítka
-    }
+    //     SDL_RenderPresent(renderer);  // Prezentace kreslítka
+    // }
+    
+    Grid grid;
+    create_grid (&grid, 20, 10);
+    int pivot;
+    
+
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
