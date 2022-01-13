e93c82d19ab3f490b92fabe7912bd023aaa64e74
Vít Vlašánek <73115311+vitvlasanek@users.noreply.github.com>
2022-01-13 14:52:24 +0100

    Update mydefs.c

--- Tetris/mydefs.c
+++ Tetris/mydefs.c
@@ -9,21 +9,18 @@ void create_grid(Grid *grid, int height, int width) {
 
 void generate_grid(Grid* grid, SDL_Renderer* renderer, int size){
     for (int y = 0; y < grid->height; y++){
-        //printf("  r = \t%d\n",y);
         for (int x = 0; x < grid->width; x++)
         {
-            //printf("%d",x);
-            // předělat
+
             int index =   y * x;
-			if (grid->cell[index] != 0)
+			if (grid->cell[index] == 0)
 			{
-                printf("tisknu");
 				SDL_Rect rect;
-				rect.x = x * size;
-				rect.y = y * size;
-				rect.h = size;
-				rect.w = size;
-				SDL_SetRenderDrawColor(renderer, 255, 0, 50, 255);
+				rect.x = (x * size)+1;
+				rect.y = (y * size)+1;
+				rect.h = size-1;
+				rect.w = size-1;
+				SDL_SetRenderDrawColor(renderer, 10, 10, 10, 255);
 	            SDL_RenderFillRect(renderer, &rect);
 			}
         }
