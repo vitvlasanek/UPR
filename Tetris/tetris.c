d8a27bb81fb3b7a008fec6819dfdc7c3484e38b2
Vít Vlašánek <73115311+vitvlasanek@users.noreply.github.com>
2022-01-14 21:51:47 +0100

    Added coloured blocks

--- Tetris/tetris.c
+++ Tetris/tetris.c
@@ -3,6 +3,12 @@
 
 int main()
 {
+    // SDL_Window* window;
+    // SDL_Renderer* renderer;
+    // pre_launch(window, renderer);
+    Colours* colours = (Colours*) malloc (sizeof(Colours));
+    set_colours(colours);
+
     if (SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
         return 1;
@@ -21,15 +27,12 @@ int main()
         SDL_Quit();
         return 1;
     }
-  
+
     Grid grid;                                              //struct s parametry herní mřížky
     create_grid (&grid, GRID_HEIGHT, GRID_WIDTH);           //vytvoření mřížky
-    printf("cislo je %d\n", grid.cell[2]);
-    int pivot;
-    
     SDL_Event event;
-    bool quit = false;
 
+    bool quit = false;
     while (!quit)
     {
         while (SDL_PollEvent(&event))
@@ -50,8 +53,14 @@ int main()
             else if (event.type == SDLK_LEFT){
 
             }
+            else if (event.type == SDLK_ESCAPE){
+                quit = true;
+            }
 
         }
+
+        SDL_SetRenderDrawColor(renderer, 48, 48, 48, 255);
+        SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);    // Nastavení barvy na červenou
         for (int i = 1; i < GRID_WIDTH + 1; i++){
             SDL_RenderDrawLine(renderer, i * (GAME_WIDTH / GRID_WIDTH), 0, i * (GAME_WIDTH / GRID_WIDTH), 600); // Vykreslení čar mřížky
@@ -60,16 +69,10 @@ int main()
             SDL_RenderDrawLine(renderer, 0, i * (GAME_HEIGHT / GRID_HEIGHT), 300, i * (GAME_HEIGHT / GRID_HEIGHT)); // Vykreslení čar mřížky
         }
         
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-        SDL_Rect right;
-        right.x = GAME_WIDTH;
-        right.y = 0;
-        right.w = GAME_WIDTH;
-        right.h = GAME_HEIGHT;
-        SDL_RenderFillRect(renderer, &right);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-        generate_grid(&grid, renderer, GAME_HEIGHT / GRID_HEIGHT);                   //generováíní mřížky (mydefs.c)
+        fill_cells(&grid, renderer, GAME_HEIGHT / GRID_HEIGHT, colours);                   //generováíní mřížky (mydefs.c)
+        fill_right(renderer);
         SDL_RenderPresent(renderer);  // Prezentace kreslítka
     }
     // SDL_DestroyRenderer(renderer);
